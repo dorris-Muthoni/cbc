05c64a0fa656dd1a3fb8e9144f549fc1300a29ad
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-19 19:03:57 +0300

    Initial commit

--- ELEARNING/urls.py
+++ ELEARNING/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='home.html'),
-   
+    path('', views.home,name='my_home'),
+    path('charts-apexcharts/', views.charts_apexcharts,name='my_charts-apexcharts'),
+    path('co-club/', views.co_club,name='my_home'),
+    path('co-events/', views.co_events,name='my_events'),
+    path('pages-blank/', views.pages_blank,name='my_pages_blank'),
+    path('pages-contact/', views.pages_contact,name='pages_contact'),
+    path('pages-error-404/', views.pages_error_404,name='my_error_404'),
+    path('pages-faq/', views.pages_faq,name='my_pages_faq'),
+    path('pages-login/', views.pages_login,name='my_pages_login'),
+    path('pages-register/', views.pages_register,name='my_pages_register'),
+    path('students-assignment-editors/', views.students_assignment_editors,name='my_students_assignment_editors'),
+    path('students-assingments/', views.students_assignments,name='my_students_assignments'),
+    path('students-badges/', views.students_badges,name='my_students_badges'),
+    path('students-class-posts/', views.students_class_posts,name='my_students_class_posts'),
+    path('students-exams/', views.students_exams,name='my_students_exams'),
+    path('students-group-projects/', views.students_group_projects,name='my_students_group_projects'),
+    path('students-live-classes/', views.students_live_classes,name='my_students_live_classes'),
+    path('students-profiles/', views.students_profiles,name='my_students_profiles'),
+    path('students-subjects/', views.students_subjects,name='my_students_subjects'),
+    path('users-profile/', views.users_profile,name='my_users_profile'),
 ]
--- ELEARNING/views.py
+++ ELEARNING/views.py
@@ -3,3 +3,60 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def charts_apexcharts(request):
+    return render(request, 'charts-apexcharts.html')
+
+def co_club(request):
+    return render(request, 'co-club.html')
+
+def co_events(request):
+    return render(request, 'co-events.html')
+
+def pages_blank(request):
+    return render(request, 'pages-blank.html')
+
+def pages_contact(request):
+    return render(request, 'pages-contact.html')
+
+def pages_error_404(request):
+    return render(request, 'pages-error-404.html')
+
+def pages_faq(request):
+    return render(request, 'pages-faq.html')
+
+def pages_login(request):
+    return render(request, 'pages-login.html')
+
+def pages_register(request):
+    return render(request, 'pages-register.html')
+
+def students_assignments(request):
+    return render(request, 'students-assignments.html')
+
+def students_assignment_editors(request):
+    return render(request, 'students-assignment-editors.html')
+
+def students_badges(request):
+    return render(request, 'students-badges.html')
+
+def students_class_posts(request):
+    return render(request, 'students-class-posts.html')
+
+def students_exams(request):
+    return render(request, 'students-exams.html')
+
+def students_group_projects(request):
+    return render(request, 'students-group-projects.html')
+
+def students_live_classes(request):
+    return render(request, 'students-live-classes.html')
+
+def students_profiles(request):
+    return render(request, 'students-profiles.html')
+
+def students_subjects(request):
+    return render(request, 'students-subjects.html')
+
+def users_profile(request):
+    return render(request, 'users-profile.html')
